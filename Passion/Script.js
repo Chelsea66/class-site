de1411be140e2760ec794eef48df1e6ed493e12b
Chelsea66 <cc66870829@gmail.com>
2022-02-01 19:52:57 -0500

    added three projects

---
+++ Passion/Script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === "Hello") {
+    x.innerHTML = "Passion!";
+  } else {
+    x.innerHTML = "Hello";
+  }
+}
+
+function Questionfunction() {
+  var x = document.getElementById("myQuestion");
+  if (x.innerHTML === "What am I interested in?") {
+    x.innerHTML = "What makes me move forward?";
+  } else {
+    x.innerHTML = "What am I interested in?";
+  }
+}
