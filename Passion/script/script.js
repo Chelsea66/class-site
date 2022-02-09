94e446a3a6b70d036354f507a610b77d07017051
Chelsea66 <cc66870829@gmail.com>
2022-02-09 09:05:28 -0500

    #Almost Finish

--- Passion/script/script.js
+++ Passion/script/script.js
@@ -15,6 +15,10 @@ function Questionfunction() {
     x.innerHTML = "What am I interested in?";
   }
 }
+
+// ABout Page
+
+
 // Their me Page
 function clickButton() {
 	var name = document.getElementById("name").value;
@@ -33,4 +37,37 @@ function addPost() {
 	posts.appendChild(li);
     
     
+}
+
+//Back to Top
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//Original Style
+function OriginalStyle(){
+	var PageStyle = document.getElementById("PageStyle");
+	PageStyle.setAttribute("class","Original_Style");
+}
+
+//Style 1
+function ChangePageStyle1(){
+	var PageStyle = document.getElementById("PageStyle");
+	PageStyle.setAttribute("class","Style1");
 }
