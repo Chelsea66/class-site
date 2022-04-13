c5a89eb51c8c8e9bb16dde0d2397efd99a09eb73
Chelsea66 <cc66870829@gmail.com>
2022-04-12 23:43:29 -0400

    add reading responses

---
+++ paper-wave/functions.js
@@ -0,0 +1,6 @@
+anime({
+  targets: '.circle',
+  translateX: 250, // -> '250px'
+  rotate: 540 // -> '540deg'
+});
+
--- paper/functions.js
+++
@@ -1,6 +0,0 @@
-anime({
-  targets: '.circle',
-  translateX: 250, // -> '250px'
-  rotate: 540 // -> '540deg'
-});
-
